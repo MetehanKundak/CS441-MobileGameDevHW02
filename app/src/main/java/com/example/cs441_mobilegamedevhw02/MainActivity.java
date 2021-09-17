956bc4b62458f481eb91bb9e8b292eecc9478892
MetehanKundak <mkundak1@binghamton.edu>
2021-09-17 11:23:15 -0400

    Second commit

--- app/src/main/java/com/example/cs441_mobilegamedevhw02/MainActivity.java
+++ app/src/main/java/com/example/cs441_mobilegamedevhw02/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.cs441_mobilegamedevhw02;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-
+    EditText hello_text;
+    Button  click_button;
+    //Button reset_button;
+    RecyclerView.Recycler recycler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hello_text = (EditText) findViewById(R.id.editTextTextPersonName );
+        recycler = (RecyclerView.RecyclerListener) findViewById(R.id.recycler);
+        click_button = (Button) findViewById(R.id.button);
+        //reset_button = (Button) findViewById(R.id.reset_button);
     }
 }
