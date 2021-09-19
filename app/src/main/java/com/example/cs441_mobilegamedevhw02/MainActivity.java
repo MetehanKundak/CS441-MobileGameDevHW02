5e3ab0734d98b460faa94d60bb5d97c9b4185135
MetehanKundak <mkundak1@binghamton.edu>
2021-09-19 18:23:35 -0400

    changed main activty

--- app/src/main/java/com/example/cs441_mobilegamedevhw02/MainActivity.java
+++ app/src/main/java/com/example/cs441_mobilegamedevhw02/MainActivity.java
@@ -10,17 +10,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-    EditText hello_text;
+    EditText hello_text = (EditText) findViewById(R.id.editTextTextPersonName);
     Button  click_button;
+    int items=0;
     //Button reset_button;
-    RecyclerView.Recycler recycler;
+    //RecyclerView.Recycler recycler;
+    public static ArrayList<recycleList> createrecycleList(int items) {
+        ArrayList<recycleList> recycleList = new ArrayList<recycleList>();
+        for (int i = 1; i <= items; i++) {
+            recycleList.add(hello_text);
+        }
+
+        return recycleList;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        hello_text = (EditText) findViewById(R.id.editTextTextPersonName );
-        recycler = (RecyclerView.RecyclerListener) findViewById(R.id.recycler);
+        //recycler = (RecyclerView.RecyclerListener) findViewById(R.id.recycler);
         click_button = (Button) findViewById(R.id.button);
         //reset_button = (Button) findViewById(R.id.reset_button);
+        RecyclerView recyclerList = (RecyclerView) findViewById(R.id.recyclerList);
+
+        // Initialize contacts
+        recyclerList = recyclerList.createList(10);
+
     }
 }
