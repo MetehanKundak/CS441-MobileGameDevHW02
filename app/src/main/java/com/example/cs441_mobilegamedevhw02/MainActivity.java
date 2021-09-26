17297c5f250c1439ed4f81b8660b465164649f84
MetehanKundak <mkundak1@binghamton.edu>
2021-09-26 18:49:22 -0400

    New commit

--- app/src/main/java/com/example/cs441_mobilegamedevhw02/MainActivity.java
+++ app/src/main/java/com/example/cs441_mobilegamedevhw02/MainActivity.java
@@ -1,22 +1,25 @@
 package com.example.cs441_mobilegamedevhw02;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.view.View.OnKeyListener;
+
+
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    EditText hello_text;
-    Button  click_button;
+    EditText input_text;
+    Button  add_button;
     //int items=0;
-    ArrayList<String> shopping_items;
+    ArrayList<EditText> shopping_items = new ArrayList<EditText>();
+    ListView list;
 
     //Button reset_button;
     //RecyclerView.Recycler recycler;
@@ -32,15 +35,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input_text = findViewById(R.id.input_text);
+        list = findViewById(R.id.list_view);
+        add_button = findViewById(R.id.button);
+
+        list.setOnKeyListener(new OnKeyListener() {
+            @Override
+            //@Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction()!=KeyEvent.ACTION_DOWN)
+                    return false;
+                if(keyCode == KeyEvent.KEYCODE_ENTER ){
+                     shopping_items.add(input_text);
+
+
+
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+
+
         //recycler = (RecyclerView.RecyclerListener) findViewById(R.id.recycler);
         //click_button = (Button) findViewById(R.id.button);
         //reset_button = (Button) findViewById(R.id.reset_button);
         //RecyclerView recyclerList = (RecyclerView) findViewById(R.id.recyclerList);
         // Initialize contacts
         //recyclerList = recyclerList.createList(10);
-        recycleview = findViewById(R.id.recyclerList);
-        input = findViewById(R.id.Input);
-        click_button = findViewById(R.id.button);
+        //recycleview = findViewById(R.id.recyclerList);
+        //input = findViewById(R.id.Input);
+        //click_button = findViewById(R.id.button);
         //recycle
 
 
