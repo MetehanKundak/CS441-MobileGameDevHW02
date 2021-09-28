4f44b27883231474012a5ba380ce2d9eee671f13
MetehanKundak <mkundak1@binghamton.edu>
2021-09-28 19:48:26 -0400

    another commit

--- app/src/main/java/com/example/cs441_mobilegamedevhw02/MainActivity.java
+++ app/src/main/java/com/example/cs441_mobilegamedevhw02/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -13,42 +15,29 @@ import android.view.View.OnKeyListener;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    EditText input_text;
-    Button  add_button;
-    //int items=0;
-    ArrayList<EditText> shopping_items = new ArrayList<EditText>();
-    ListView list;
+EditText editText;
+//List<items> items_list = new ArrayList<>();
 
-    //Button reset_button;
-    //RecyclerView.Recycler recycler;
-   /* public static ArrayList<recycleList> createrecycleList(int items) {
-        ArrayList<recycleList> recycleList = new ArrayList<recycleList>();
-        for (int i = 1; i <= items; i++) {
-            recycleList.add(hello_text);
-        }
-
-        return recycleList;
-    }*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        input_text = findViewById(R.id.input_text);
-        list = findViewById(R.id.list_view);
-        add_button = findViewById(R.id.button);
+        //fillitemsList();
 
-        list.setOnKeyListener(new OnKeyListener() {
+
+        editText = findViewById(R.id.editText);
+        editText.setOnKeyListener(new OnKeyListener() {
             @Override
-            //@Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (event.getAction()!=KeyEvent.ACTION_DOWN)
                     return false;
                 if(keyCode == KeyEvent.KEYCODE_ENTER ){
-                     shopping_items.add(input_text);
-
+                    //your necessary codes...
 
 
 
@@ -61,20 +50,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        //recycler = (RecyclerView.RecyclerListener) findViewById(R.id.recycler);
-        //click_button = (Button) findViewById(R.id.button);
-        //reset_button = (Button) findViewById(R.id.reset_button);
-        //RecyclerView recyclerList = (RecyclerView) findViewById(R.id.recyclerList);
-        // Initialize contacts
-        //recyclerList = recyclerList.createList(10);
-        //recycleview = findViewById(R.id.recyclerList);
-        //input = findViewById(R.id.Input);
-        //click_button = findViewById(R.id.button);
-        //recycle
-
-
-
-
-
     }
+/*
+    private void fillitemsList() {
+        //int id;
+        //String name;
+        items i0 = new items(0, "apple");
+        items i1 = new items(1, "pineapple");
+        items_list.addAll(Arrays.asList(i0,i1));
+    }
+*/
+
 }
